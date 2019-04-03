37a7cea20f6b7d01a5f630f492fc1bb906853a30
lenonid-sosh <lenya.soschnikov@gmail.com>
2019-04-03 22:31:26 +0300

    Soshnikov_guessNumber

--- app/src/main/java/com/example/number/MainActivity.java
+++ app/src/main/java/com/example/number/MainActivity.java
@@ -6,12 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvInfo;
     EditText etInput;
     Button bControl;
+    Random rand = new Random();
+
+    int numberToGuess = rand.nextInt(100);
+    int guess;
+    boolean win = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +31,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        int value = Integer.parseInt(etInput.getText().toString());
-        tvInfo.setText(getResources().getString(R.string.ahead));
+        if (!win){
+
+            if (etInput.getText().toString().equals("")){
+                tvInfo.setText("Вы не ввели число!");
+            }
+            else {
+                guess = Integer.parseInt(etInput.getText().toString());
+
+                if (guess > numberToGuess)
+                    tvInfo.setText(getResources().getString(R.string.ahead));
+                if (guess < numberToGuess)
+                    tvInfo.setText(getResources().getString(R.string.behind));
+                if (guess == numberToGuess)
+                {
+                    tvInfo.setText(getResources().getString(R.string.hit));
+                    bControl.setText(getResources().getString(R.string.play_more));
+                    win = true;
+                }
+            }
+
+        }
+        else
+        {
+            numberToGuess = rand.nextInt(100);
+            bControl.setText(getResources().getString(R.string.input_value));
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            win = false;
+        }
+        etInput.setText("");
+
     }
-}
+
+
+    }
+
 
